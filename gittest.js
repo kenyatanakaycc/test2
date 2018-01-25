12c14aa95ac123a91d2313248ed4927f7b973dd3
kenyatanakaycc <33506931+kenyatanakaycc@users.noreply.github.com>
2018-01-25 11:48:50 +0900

    Add files via upload

---
+++ gittest.js
@@ -0,0 +1,7 @@
+function StoppedWorkflowStateAlert(){
+    var WFState = '';
+    WFState = getFormElement(SearchKeyff38, Text);
+    if(WFState == "Žæ‰º‚°"){
+        $("#labelStopButton1").hide();$("#labelStopButton2").hide();// ƒ{ƒ^ƒ“‚ð‰B‚·
+    }
+}
